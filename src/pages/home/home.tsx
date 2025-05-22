ff3dd183e45e5ba47f96d5b7e99d1519e48beb8d
hubluish <james2048@naver.com>
2025-05-22 12:40:59 +0900

    [FEAT] coloroption 컴포넌트

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import ColorOption from '../../components/section/home/ColorOption';
+
+function Hello() {
+    return (
+        <main>
+    <ColorOption
+        title="Monotone"
+        description="Neutral tones that keep it simple and clean."
+    />
+    </main>
+    );
+}
+
+export default Hello;
