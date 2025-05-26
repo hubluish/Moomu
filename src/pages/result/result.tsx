d656ec42fc806e63ffa0cbd15cda734c61fdc1a9
hubluish <james2048@naver.com>
2025-05-26 22:34:47 +0900

    [FEAT] Title 컴포넌트 추가

---
+++ src/pages/result/result.tsx
@@ -0,0 +1,9 @@
+import MoodboardTitle from '../../components/result/MoodboardTitle';
+
+export default function ResultPage() {
+    return (
+        <main>
+        <MoodboardTitle />
+        </main>
+    );
+}
