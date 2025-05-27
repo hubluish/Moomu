9f60da31264405a4bc816295d71972c8b79b3678
hubluish <james2048@naver.com>
2025-05-27 17:47:51 +0900

    [FEAT] ActionButton 컴포넌트 추가

--- src/pages/result/result.tsx
+++ src/pages/result/result.tsx
@@ -1,13 +1,23 @@
 import MoodboardTitle from '../../components/result/MoodboardTitle';
 import Chips from '../../components/result/Chips';
+import ActionButtons from '../../components/result/ActionButtons';
+import styles from './result.module.css';
 import React from 'react';
-
 
 export default function ResultPage() {
     return (
         <main>
         <MoodboardTitle />
-        <Chips tags={['Pastel', 'Minimal', 'handwritten', 'Dreamy']} />
+        <div className={styles.topWrapper}>
+            <div className={styles.chips}>
+                <Chips tags={['Pastel', 'Minimal', 'handwritten', 'Dreamy']} />
+            </div>
+            <div className={styles.actionsWrapper}>
+                <div className={styles.actions}>
+                    <ActionButtons />
+                </div>
+            </div>
+        </div>
         </main>
     );
 }
