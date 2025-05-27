cae6ff941b1a420879f08d98bf2bd91c9131394c
hubluish <james2048@naver.com>
2025-05-27 17:19:04 +0900

    [FEAT] Chips 컴포넌트 제작

--- src/pages/result/result.tsx
+++ src/pages/result/result.tsx
@@ -1,9 +1,13 @@
 import MoodboardTitle from '../../components/result/MoodboardTitle';
+import Chips from '../../components/result/Chips';
+import React from 'react';
+
 
 export default function ResultPage() {
     return (
         <main>
         <MoodboardTitle />
+        <Chips tags={['Pastel', 'Minimal', 'handwritten', 'Dreamy']} />
         </main>
     );
 }
