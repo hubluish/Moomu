31b4e2ba4ec1592d2b3b2ee2f4ddf0a0a859ffff
GGUM <xxoyya24@soongsil.ac.kr>
2025-05-28 15:38:21 +0900

    [FEAT} Bottom Sheet 개발
    [FEAT} Bottom Sheet 개발
    
    Bottom Sheet 기능을 개발하였습니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "../styles/variable.css";
+import Bottom from "@/components/common/bottom/bottom";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Bottom />
       </body>
     </html>
   );
