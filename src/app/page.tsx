65facb90466b16c33b93d1e20d49ca879e69c08a
hubluish <james2048@naver.com>
2025-06-12 07:19:40 +0900

    [FIX] 파일 구조 변경

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function HomeRedirect() {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/homeIntro');
+  }, []);
+  return null;
+}
