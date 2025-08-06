f8ee9bee4a02c3fa307e487ef86cc31fb4d3f1ef
hubluish <james2048@naver.com>
2025-08-06 14:20:43 +0900

    [FIX] 이미지 도메인 오류 해결

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: [
       "i.pinimg.com",
+      "noonnucc-production.sfo2.cdn.digitaloceanspaces.com",
       // 필요하다면 다른 외부 이미지 도메인도 추가
     ],
   },
