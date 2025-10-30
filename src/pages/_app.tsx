1bcf108eb2820fa551e49e10294921a6e180fd63
gaeun2o <gaeun.pop@gmail.com>
2025-10-30 12:32:35 +0900

    [feat] Google Analytics 추가
    _app.tsx는 원래 파일로 복구하고
    layout.tsx에 추가하였습니다

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,10 @@
 import "@/app/globals.css";
 import Header from "@/components/common/header/header";
 import type { AppProps } from "next/app";
-import Script from "next/script";
-
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      {/* Google Analytics */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-V50JJSBVK4"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-V50JJSBVK4');
-        `}
-      </Script>
-      
       <Header />
       <Component {...pageProps} />
     </>
