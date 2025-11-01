907915236f26dc808e81746207a43c3f45f3020f
hubluish <james2048@naver.com>
2025-11-01 20:18:19 +0900

    style: step 선택부분 그리드, 타이틀 크기 수정

--- src/components/section/generate/TitleBlock.tsx
+++ src/components/section/generate/TitleBlock.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './TitleBlock.module.css';
 
 interface TitleBlockProps {
   title: React.ReactNode;
@@ -7,36 +8,11 @@ interface TitleBlockProps {
 
 const TitleBlock: React.FC<TitleBlockProps> = ({ title, subtitle }) => {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>{title}</h1>
-      <p style={styles.subtitle}>{subtitle}</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.subtitle}>{subtitle}</p>
     </div>
   );
-};
-
-const styles: { [key: string]: React.CSSProperties } = {
-  container: {
-    margin: '150px auto 0 auto',
-    textAlign: 'center',
-  },
-  title: {
-    color: '#000',
-    fontFamily: 'Pretendard',
-    fontSize: '45px',
-    fontStyle: 'normal',
-    fontWeight: 800,
-    lineHeight: 'normal',
-    marginBottom: '24px',
-  },
-  subtitle: {
-    color: '#000',
-    fontFamily: 'Pretendard',
-    fontSize: '20px',
-    fontStyle: 'normal',
-    fontWeight: 400,
-    lineHeight: 'normal',
-    margin: 0,
-  },
 };
 
 export default TitleBlock;
