5114970517667a00753f446b9d08fe51c0f190ce
hubluish <james2048@naver.com>
2025-11-02 19:34:27 +0900

    style: Imagebox 반응형, Titlebox 임시로 반응형 크기만 변경

--- src/components/section/result/ImageGridSkeleton.tsx
+++ src/components/section/result/ImageGridSkeleton.tsx
@@ -1,9 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import styles from './ImageGridSkeleton.module.css';
 
 const ImageGridSkeleton = () => {
+  const [numberOfItems, setNumberOfItems] = useState(9);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 480) {
+        setNumberOfItems(8);
+      } else {
+        setNumberOfItems(9);
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className={styles.skeletonGrid}>
-      {Array.from({ length: 9 }).map((_, index) => (
+      {Array.from({ length: numberOfItems }).map((_, index) => (
         <div key={index} className={styles.skeletonItem}></div>
       ))}
     </div>
